bab9ffe5b195eba0a0ecbfd80360a552d6c4edac
Shubham807621 <shubhammusale111@gmail.com>
2026-05-29 18:48:38 +0530

    implement fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -12,11 +12,11 @@ export default function App() {
     <SafeAreaView className="flex-1 items-center justify-center bg-surface-lowest">
       {/* <SplashScreen /> */}
 
-      <Text className="text-[50px]  font-bold text-primary ">KINETIX</Text>
+      <Text className="text-[50px] font-sans-bold text-primary ">KINETIX</Text>
 
       <Link href="/(auth)/log-in" className="mt-10 px-14 py-4 rounded-full bg-surface-container-low shadow-xl ">
         <Text
-          className=" text-[16px] font-bold uppercase text-primary"
+          className=" text-[16px] font-sans-bold uppercase text-primary"
           style={{
             letterSpacing: 2,
           }}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,30 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
 import '@/global.css';
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  const [fontLoaded] = useFonts({
+    "sans-light": require("../assets/fonts/PlusJakartaSans-Light.ttf"),
+    "sans-medium": require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
+    "sans-regular": require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
+    "sans-semibold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+    "sans-bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
+    "sans-extrabold": require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf")
+  });
+
+
+
+  useEffect(() => {
+    if (fontLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontLoaded]);
+
+
+  if (!fontLoaded) {
+    return null;
+  }
+
   return <Stack screenOptions={{ headerShown: false }} />;
 }
